14584cadd2f88009c8a849d48e8348abe7d47738
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-05 17:22:14 +0330

    feat(general): create a simple websocket event between client and server

--- lesson_01_Websocket/app.js
+++ lesson_01_Websocket/app.js
@@ -18,6 +18,23 @@ const wss = new webSocket.Server({
 wss.on("headers", (headers, req) => {
     console.log(headers);
 });
+
+/**
+ * listen to websocket connection event
+ * connection events happens if user connected to the socket
+ */
+wss.on('connection', (socket, req) => {
+    console.log("new soccket connected");
+
+    /** listen to websocket client message even */
+    socket.on('message', (msg) => {
+        console.log(msg);
+
+        /** send message to client */
+        socket.send("Hi, welcome to my server");
+    });
+});
+
 /** define connection port */
 const PORT = 8000;
 
