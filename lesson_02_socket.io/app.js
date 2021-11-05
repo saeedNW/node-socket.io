77fabb55e710f00810cd1dcf150d6dae57facf52
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-05 17:38:54 +0330

    feat(app.js): create http server

---
+++ lesson_02_socket.io/app.js
@@ -0,0 +1,13 @@
+/** import http module */
+const http = require("http");
+
+/** create http server */
+const server = http.createServer();
+
+/** define connection port */
+const PORT = 8000;
+
+/** start server */
+server.listen(PORT, () => {
+    console.log(`application is running on port: ${PORT}`);
+});
