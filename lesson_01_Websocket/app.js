3a5e49add45afa151d532a3af00eede2f46298b9
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-05 17:19:51 +0330

    feat(app.js): create websocket server

--- lesson_01_Websocket/app.js
+++ lesson_01_Websocket/app.js
@@ -1,13 +1,27 @@
 /** import http module */
 const http = require("http");
+/** import websocket */
+const webSocket = require("ws");
 
 /** create http server */
 const server = http.createServer();
 
+/** create websocket server */
+const wss = new webSocket.Server({
+    server,
+});
+
+/**
+ * listen to websocket headers event
+ * headers event returns handshake prosecc headers
+ */
+wss.on("headers", (headers, req) => {
+    console.log(headers);
+});
 /** define connection port */
-const PORT = 8000
+const PORT = 8000;
 
 /** start server */
 server.listen(PORT, () => {
-    console.log(`application is running on port: ${PORT}`)
+    console.log(`application is running on port: ${PORT}`);
 });
