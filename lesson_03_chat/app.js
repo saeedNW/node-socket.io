a3f2cdd23a95112b60023f3c74fa416cb5471bbb
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-06 20:00:06 +0330

    feat(app.js): create chat message event listener and emitter

--- lesson_03_chat/app.js
+++ lesson_03_chat/app.js
@@ -23,3 +23,18 @@ const io = socketIo(server, {
         origin: '*'
     }
 });
+
+/**
+ * listen to socket.io connection event
+ * connection events happens if user connected to the socket
+ */
+io.on('connection', (socket) => {
+    console.log("new socket connected" + socket.id);
+
+    /** socket event listener for getting messages */
+    socket.on("chat_message", msg => {
+        /** socket event emitter for sending received message to everyone */
+        io.emit("chat_message", msg);
+    })
+});
+
