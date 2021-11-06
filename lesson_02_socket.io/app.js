d43239ed4a624c0ba7f6c42415dec1dc1c461c0b
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-06 12:47:38 +0330

    feat(app.js): create socket.io server

--- lesson_02_socket.io/app.js
+++ lesson_02_socket.io/app.js
@@ -1,8 +1,24 @@
 /** import http module */
 const http = require("http");
 
+/** import socket.io */
+const socketIo = require("socket.io")
+
 /** create http server */
 const server = http.createServer();
+
+/** create websocket server */
+const io = socketIo(server);
+
+/**
+ * listen to socket.io connection event
+ * connection events happens if user connected to the socket
+ */
+io.on('connection', (socket) => {
+    console.log("new soccket connected");
+
+
+});
 
 /** define connection port */
 const PORT = 8000;
