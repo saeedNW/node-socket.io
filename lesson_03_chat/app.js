9e796b5e0040d95c14f50f6950c898313874740f
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-06 19:10:46 +0330

    feat(app.js): define express static directory

--- lesson_03_chat/app.js
+++ lesson_03_chat/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 /** create application from express */
 const app = express();
 
+/** define express static */
+app.use(express.static(__dirname + '/public'));
+
 /** define connection port */
 const PORT = 8000;
 
