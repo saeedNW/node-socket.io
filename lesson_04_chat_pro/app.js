6f0eba434012d910c316771d18fe05e07c59af6d
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-09 14:12:41 +0330

    feat(app.js): create application and start application and socket server

---
+++ lesson_04_chat_pro/app.js
@@ -0,0 +1,26 @@
+/** import http module */
+const express = require("express");
+/** import socket.io */
+const socketIo = require("socket.io");
+
+/** create application from express */
+const app = express();
+
+/** define express static */
+app.use(express.static(__dirname + '/public'));
+
+/** define connection port */
+const PORT = 8000;
+
+/** start server */
+const server = app.listen(PORT, () => {
+    console.log(`application is running on port: ${PORT}`);
+});
+
+/** create socket.io server */
+const io = socketIo(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
