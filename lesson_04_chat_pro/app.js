3bcc65125268bf1afb23dc7196f533dd702a80b4
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-09 15:13:47 +0330

    feat(app.js): create socket connection and send namespaces to client

--- lesson_04_chat_pro/app.js
+++ lesson_04_chat_pro/app.js
@@ -2,6 +2,8 @@
 const express = require("express");
 /** import socket.io */
 const socketIo = require("socket.io");
+/** import socket structure */
+const structure = require("./socket/structure");
 
 /** create application from express */
 const app = express();
@@ -24,3 +26,19 @@ const io = socketIo(server, {
     }
 });
 
+/**
+ * listen to socket.io connection event
+ * connection events happens if user connected to the socket
+ */
+io.on('connection', (socket) => {
+    /** save namespacess data */
+    const nsDate = structure.map((namespace) => {
+        return {
+            title: namespace.title,
+            endpoint: namespace.endpoint
+        }
+    });
+
+    /** socket event emitter for sending namespaces info to connected socket */
+    socket.emit('nameSpaceLoad', nsDate);
+});
