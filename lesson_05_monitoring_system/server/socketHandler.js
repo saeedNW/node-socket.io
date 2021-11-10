8dcf2a7a05079f44f926ef15a72f0c59aaa45a93
Saeed Noroozi <saeednorouzi98@gmail.com>
2021-11-10 14:27:31 +0330

    feat(server): send system info from socket handler to front end statics

--- lesson_05_monitoring_system/server/socketHandler.js
+++ lesson_05_monitoring_system/server/socketHandler.js
@@ -1,7 +1,12 @@
 module.exports = (io) => {
     io.on('connection', (socket) => {
         socket.on('dataFromPc', (systemInfo) => {
-            console.log(systemInfo)
-        })
+            systemInfo.id = socket.id;
+            io.to('monitoring').emit('pcData', systemInfo);
+        });
+
+        socket.on('joinMonitoringRoom', () => {
+            socket.join('monitoring')
+        });
     });
 }
